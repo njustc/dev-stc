4fefedaa983ac2c838848fc5f1f9e8d927549009
Junju Song <junju@Chuns-MacBook-Pro.local>
2018-05-12 16:42:07 +0800

    Add consign list for User

--- server/src/test/java/com/sinosteel/activiti/ConsignActivitiTest.java
+++ server/src/test/java/com/sinosteel/activiti/ConsignActivitiTest.java
@@ -1,70 +1,70 @@
-package com.sinosteel.activiti;
-
-import com.sinosteel.FrameworkApplication;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.*;
-
-/**
- * @author LBW
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(FrameworkApplication.class)
-public class ConsignActivitiTest {
-
-    private ConsignActiviti consignActiviti = new ConsignActiviti();
-
-    @Before
-    public void deployConsignActiviti() {
-        consignActiviti.deploy();
-    }
-    @Test
-    public void newConsign() {
-
-        String processInstanceID =  consignActiviti.createConsignProcess("0", "0");
-        assertNotNull(processInstanceID);
-        System.out.println("委托实例成功创建。 ProcessInstanceID: " + processInstanceID);
-
-        String queryResult = consignActiviti.getProcessState(processInstanceID);
-        assertNotNull(queryResult);
-        System.out.println(queryResult);
-
-        System.out.println("正在测试提交委托");
-        consignActiviti.submitConsign(processInstanceID, "0");
-        System.out.println(consignActiviti.getProcessState(processInstanceID));
-
-        consignActiviti.setWorker(processInstanceID, "1");
-
-        System.out.println("正在测试否决委托");
-        consignActiviti.checkConsign(false, processInstanceID, "1" );
-        System.out.println(consignActiviti.getProcessState(processInstanceID));
-
-        System.out.println("正在测试提交委托");
-        consignActiviti.submitConsign(processInstanceID, "0");
-        System.out.println(consignActiviti.getProcessState(processInstanceID));
-
-        consignActiviti.setWorker(processInstanceID, "1");
-
-        System.out.println("正在测试通过委托");
-        consignActiviti.checkConsign(true, processInstanceID, "1" );
-        System.out.println(consignActiviti.getProcessState(processInstanceID));
-    }
-
-
-
-    @Test
-    public void getUserTasks() {
-    }
-
-    @Test
-    public void submitConsign() {
-    }
-
-    @Test
-    public void checkConsign() {
-    }
-}
+//package com.sinosteel.activiti;
+//
+//import com.sinosteel.FrameworkApplication;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * @author LBW
+// */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(FrameworkApplication.class)
+//public class ConsignActivitiTest {
+//
+//    private ConsignActiviti consignActiviti = new ConsignActiviti();
+//
+//    @Before
+//    public void deployConsignActiviti() {
+//        consignActiviti.deploy();
+//    }
+//    @Test
+//    public void newConsign() {
+//
+//        String processInstanceID =  consignActiviti.createConsignProcess("0", "0");
+//        assertNotNull(processInstanceID);
+//        System.out.println("委托实例成功创建。 ProcessInstanceID: " + processInstanceID);
+//
+//        String queryResult = consignActiviti.getProcessState(processInstanceID);
+//        assertNotNull(queryResult);
+//        System.out.println(queryResult);
+//
+//        System.out.println("正在测试提交委托");
+//        consignActiviti.submitConsign(processInstanceID, "0");
+//        System.out.println(consignActiviti.getProcessState(processInstanceID));
+//
+//        consignActiviti.setWorker(processInstanceID, "1");
+//
+//        System.out.println("正在测试否决委托");
+//        consignActiviti.checkConsign(false, processInstanceID, "1" );
+//        System.out.println(consignActiviti.getProcessState(processInstanceID));
+//
+//        System.out.println("正在测试提交委托");
+//        consignActiviti.submitConsign(processInstanceID, "0");
+//        System.out.println(consignActiviti.getProcessState(processInstanceID));
+//
+//        consignActiviti.setWorker(processInstanceID, "1");
+//
+//        System.out.println("正在测试通过委托");
+//        consignActiviti.checkConsign(true, processInstanceID, "1" );
+//        System.out.println(consignActiviti.getProcessState(processInstanceID));
+//    }
+//
+//
+//
+//    @Test
+//    public void getUserTasks() {
+//    }
+//
+//    @Test
+//    public void submitConsign() {
+//    }
+//
+//    @Test
+//    public void checkConsign() {
+//    }
+//}
---
+++ server/src/test/java/com/sinosteel/service/AuthTests.java
@@ -0,0 +1,119 @@
+package com.sinosteel.service;
+
+import com.sinosteel.FrameworkApplication;
+import com.sinosteel.domain.Consign;
+import com.sinosteel.domain.Function;
+import com.sinosteel.domain.Role;
+import com.sinosteel.domain.User;
+import com.sinosteel.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(FrameworkApplication.class)
+public class AuthTests {
+
+    private User marketuser;
+    private User custom1user;
+    private User custom2user;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void testGetUser()
+    {
+        System.out.println("获取市场部工作人员账号测试");
+        marketuser = userRepository.findByUsername("marketing");
+        if(marketuser != null)
+            System.out.println("获取市场部工作人员账号成功");
+        else
+            System.out.println("获取市场部工作人员账号失败");
+
+        System.out.println("获取普通用户1账号测试");
+        custom1user = userRepository.findByUsername("customer1");
+        if(custom1user != null)
+            System.out.println("获取普通用户账号1成功");
+        else
+            System.out.println("获取普通用户账号1失败");
+
+        System.out.println("获取普通用户2账号测试");
+        custom2user = userRepository.findByUsername("customer2");
+        if(custom2user != null)
+            System.out.println("获取普通用户账号2成功");
+        else
+            System.out.println("获取普通用户账号2失败");
+
+    }
+    @Test
+    public void testUserRole()
+    {
+        System.out.println("获取用户对应角色测试");
+        List<Role> marketroles = marketuser.getRoles();
+        for(Role role : marketroles)
+        {
+            System.out.println("市场部工作人员role_String = "+ role.getRoleString());
+        }
+        List<Role> customer1roles = custom1user.getRoles();
+        for(Role role : customer1roles)
+        {
+            System.out.println("普通客户1role_String = " + role.getRoleString());
+        }
+        List<Role> customer2roles = custom2user.getRoles();
+        for(Role role : customer2roles)
+        {
+            System.out.println("普通客户2role_String = " + role.getRoleString());
+        }
+
+    }
+    @Test
+    public void testUserFunction()
+    {
+        System.out.println("测试用户功能");
+        List<Function> marketuserFunctions = marketuser.getFunctions();
+        for(Function function : marketuserFunctions)
+        {
+            System.out.println("市场部工作人员函数 = " + function.getFunctionString());
+        }
+
+        List<Function> customuserFunctions = custom1user.getFunctions();
+        for(Function function : customuserFunctions)
+        {
+            System.out.println("客户1函数 = " + function.getFunctionString());
+        }
+
+        List<Function> customuser2Functions = custom2user.getFunctions();
+
+        for(Function function : customuser2Functions)
+        {
+            System.out.println("客户2函数 = "+function.getFunctionString());
+        }
+    }
+    @Test
+    public void testUserConsign()
+    {
+        System.out.println("测试用户委托");
+        List<Consign> customer1Consigns = custom1user.getConsigns();
+        if(customer1Consigns == null)
+            System.out.println("客户1没有委托");
+        else {
+            for (Consign consign : customer1Consigns) {
+                System.out.println("客户1委托\n" + consign.getConsignation());
+            }
+        }
+        List<Consign> customer2Consigns = custom2user.getConsigns();
+        if(customer2Consigns == null)
+            System.out.println("客户2没有委托");
+        else {
+            for (Consign consign : customer2Consigns) {
+                System.out.println("客户2委托\n" + consign.getConsignation());
+            }
+        }
+    }
+
+}
