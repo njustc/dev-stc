66d664e09aee3d417264efdf89c22d30bbf1cb6b
LeBW <27091925@qq.com>
2018-05-13 23:51:40 +0800

    Add Swagger to Project for API.

---
+++ server/src/main/java/com/sinosteel/framework/config/swagger/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.sinosteel.framework.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author LBW
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.sinosteel.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
