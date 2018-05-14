59fd74a6825d27dd923ddfd54b7fd77b14c0ce9f
lllwwwbbb <151220068@smail.nju.edu.cn>
2018-05-15 02:52:32 +0800

    add deleteConsign

--- client/framework-webclient/src/routes/Consign/containers/StaffConsignListContainer.js
+++ client/framework-webclient/src/routes/Consign/containers/StaffConsignListContainer.js
@@ -4,7 +4,7 @@ import {connect} from "react-redux";
 import {addTabAction, setState} from "../../../modules/ducks/Layout";
 import {setConsignList, setConsignStatus, setFilter} from "../../../modules/ducks/Consign"
 import {StaffConsignContentView} from "ROUTES/Consign";
-import {httpGet} from "UTILS/FetchUtil";
+import {httpDelete, httpGet} from "UTILS/FetchUtil";
 
 // todo: 利用第二个参数ownProps来过滤，实现搜索，ownProps是被显示传入的属性值，不包括map进去的
 const mapStateToProps = (state, ownProps) => {
@@ -35,7 +35,25 @@ const mapDispatchToProps = (dispatch, ownProps) => {
             });
         },
         deleteConsign: (id) => {
-            console.log('delete', id);
+            httpDelete('http://127.0.0.1:8000/services/consign', {id:id}, (result) => {
+                const {status} = result;
+                if (status === 'SUCCESS') {
+                    httpGet('http://127.0.0.1:8000/services/consign', (result) => {
+                        const {status, data} = result;
+                        if (status === 'SUCCESS') {
+                            dispatch(setConsignList(data));
+                            for (let i = 0; i < data.length; i ++) {
+                                httpGet('http://localhost:8000/services/consignActiviti/' + data[i].processInstanceID, (result) => {
+                                    const {status, data} = result;
+                                    if (status === 'SUCCESS') {
+                                        dispatch(setConsignStatus(i, data.state));
+                                    }
+                                })
+                            }
+                        }
+                    });
+                }
+            });
         },
     }
 };
