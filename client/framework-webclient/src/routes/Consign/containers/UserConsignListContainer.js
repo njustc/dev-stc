e3e5520c1b1fd1c782476481d324ca14c787c3f6
lllwwwbbb <151220068@smail.nju.edu.cn>
2018-05-14 18:54:51 +0800

    move render to tmp

--- client/framework-webclient/src/routes/Consign/containers/UserConsignListContainer.js
+++ client/framework-webclient/src/routes/Consign/containers/UserConsignListContainer.js
@@ -4,25 +4,6 @@ import {connect} from "react-redux";
 import {addTabAction, setState} from "../../../modules/ducks/Layout";
 import {setFilter} from "../../../modules/ducks/Consign"
 import UserConsignContentView from "ROUTES/Consign/components/ConsignContentComponent";
-import StaffConsignContentContainer from "ROUTES/Consign/containers/StaffConsignContentContainer";
-
-const  containsPane = (key, panes) => {
-    for(let i=0; i<panes.length; i++) {
-        if(key === panes[i].key) {
-            return true;
-        }
-    }
-    return false;
-};
-
-const addTab = (Panes, key, name, component, dispatch) => {
-    const panes = Panes;
-    const activeKey = key;
-    if(!containsPane(key, Panes)){
-        panes.push({ title: name, content: React.createElement(component), key: activeKey });
-    }
-    dispatch(setState({ panes,activeKey }))
-};
 
 // todo: 利用第二个参数ownProps来过滤，实现搜索，ownProps是被显示传入的属性值，不包括map进去的
 const mapStateToProps = (state, ownProps) => {
@@ -34,21 +15,11 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
-        addTab: (Panes, key, name, component) => {
-            const panes = Panes;
-            const activeKey = key;
-            if(!containsPane(key, Panes)){
-                panes.push({ title: name, content: React.createElement(component), key: activeKey });
-            }
-            dispatch(setState({ panes,activeKey }))
-        },
         showContent: () => dispatch(addTabAction('details', '委托详情', UserConsignContentView)),
-        // showContent: (panes) => addTab(panes,'details','委托详情',StaffConsignContentContainer, dispatch),
-        // showContent: (panes) => addTab(panes,'details','委托详情',UserConsignContentView, dispatch),
         newConsign: () => {
             //message.success('This is a prompt message for success, and it will disappear in 10 seconds', 10);
         },
-        setListFilter: (newlistFilter) => dispatch(setFilter(newlistFilter))
+        setListFilter: (newlistFilter) => dispatch(setFilter(newlistFilter)),
     }
 };
 
