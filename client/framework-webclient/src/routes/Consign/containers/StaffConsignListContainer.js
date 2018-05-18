5f669376ba1c103bfa18f7f76fac9c588769f9a3
lllwwwbbb <151220068@smail.nju.edu.cn>
2018-05-18 15:43:45 +0800

    remove TODO

--- client/framework-webclient/src/routes/Consign/containers/StaffConsignListContainer.js
+++ client/framework-webclient/src/routes/Consign/containers/StaffConsignListContainer.js
@@ -6,15 +6,14 @@ import {setConsignIndex, setConsignState, setFilter} from "MODULES/ducks/Consign
 import {StaffConsignContentView} from "ROUTES/Consign";
 import {deleteConsign, getConsignList} from "SERVICES/ConsignService";
 
-// todo: 利用第二个参数ownProps来过滤，实现搜索，ownProps是被显示传入的属性值，不包括map进去的
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
     return {
         dataSource: state.Consign.list.filter(state.Consign.listFilter),
         enableNew: false,
     }
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch) => {
     return {
         showContent: (index) => {
             dispatch(addTabAction('委托详情', StaffConsignContentView));
--- client/framework-webclient/src/routes/Consign/containers/UserConsignListContainer.js
+++ client/framework-webclient/src/routes/Consign/containers/UserConsignListContainer.js
@@ -12,15 +12,14 @@ import {UserConsignContentView} from "ROUTES/Consign";
 import {deleteConsign, getConsignList, newConsign} from "SERVICES/ConsignService";
 import {addTabAction} from "MODULES/ducks/Layout";
 
-// todo: 利用第二个参数ownProps来过滤，实现搜索，ownProps是被显示传入的属性值，不包括map进去的
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
     return {
         dataSource: state.Consign.list.filter(state.Consign.listFilter),
         enableNew: true,
     }
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch) => {
     return {
         showContent: (index) => {
             dispatch(addTabAction('委托详情', UserConsignContentView));
