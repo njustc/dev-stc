42e6241a2de6100bb43472042d3cdfc0857c61df
tangcong <1195882464@qq.com>
2018-05-19 11:25:12 +0800

    fix

--- server/src/main/java/com/sinosteel/service/ConsignService.java
+++ server/src/main/java/com/sinosteel/service/ConsignService.java
@@ -28,13 +28,7 @@ public class ConsignService extends BaseService<Consign> {
 
 
 
-<<<<<<< HEAD
-    public JSON queryConsigns(User user)throws Exception
-    {
-=======
-    public JSON queryConsigns(User user) throws Exception {
->>>>>>> 6a7aef396c9f9b81231f9b1c9ef07febea40d1ba
-
+    public JSON queryConsigns(User user) {
         if (user != null)
             System.out.println("queryConsigns--> query user role: " + user.getRoles().get(0).getRoleName());
         if (user.getRoles().get(0).getRoleName().equals("普通客户"))
@@ -76,11 +70,7 @@ public class ConsignService extends BaseService<Consign> {
     }
 
     //增加委托
-<<<<<<< HEAD
-    public JSONObject addConsign(JSONObject params,List<MultipartFile> files,User user)throws Exception
-=======
-    public JSONObject addConsign(JSONObject params,List<MultipartFile> files,User user) throws Exception
->>>>>>> 6a7aef396c9f9b81231f9b1c9ef07febea40d1ba
+    public JSONObject addConsign(JSONObject params,List<MultipartFile> files,User user)
     {
 
         String uid=UUID.randomUUID().toString();
@@ -109,12 +99,8 @@ public class ConsignService extends BaseService<Consign> {
     }
 
 
-<<<<<<< HEAD
 
-    private JSONObject processConsign(Consign consign)throws Exception {
-=======
-    private JSONObject processConsign(Consign consign) throws Exception {
->>>>>>> 6a7aef396c9f9b81231f9b1c9ef07febea40d1ba
+    private JSONObject processConsign(Consign consign) {
         //增加委托状态
         String processState = (String)processInstanceService.queryProcessState(consign.getProcessInstanceID()).get("state");
         JSONObject jsonObject = JSON.parseObject(JSONObject.toJSONString(consign));
@@ -123,11 +109,7 @@ public class ConsignService extends BaseService<Consign> {
 
     }
 
-<<<<<<< HEAD
-    private  JSONArray processConsigns(List<Consign> consigns)throws Exception {
-=======
-    private  JSONArray processConsigns(List<Consign> consigns) throws Exception {
->>>>>>> 6a7aef396c9f9b81231f9b1c9ef07febea40d1ba
+    private  JSONArray processConsigns(List<Consign> consigns){
         JSONArray resultArray = new JSONArray();
         //去掉委托内容,添加状态
         for (Consign consign: consigns) {
