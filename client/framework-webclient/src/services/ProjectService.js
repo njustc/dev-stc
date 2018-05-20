1841fc48e6dd465dcbc5b7aa586f347489fcddbc
Jie Yang <jieyang@JiedeMacBook-Pro.local>
2018-05-20 16:23:17 +0800

    finished ProjectService

--- client/framework-webclient/src/services/ProjectService.js
+++ client/framework-webclient/src/services/ProjectService.js
@@ -1,31 +1,62 @@
-import {setProjectList} from "MODULES/ducks/Project";
-import {STATUS} from "SERVICES/common";
+import {removeProject, setProjectContent, setProjectList} from "MODULES/ducks/Project";
+import {baseServiceAddress, STATUS} from "SERVICES/common";
+import {httpDelete, httpGet, httpPost, httpPut} from "UTILS/FetchUtil";
+import {addConsign, removeConsign, setConsignContent} from "MODULES/ducks/Consign";
+
+/*ToDo*/
+/*接口需要与后台确认*/
+/*注意每一个http方法的url*/
+
+const projectBase = baseServiceAddress + '/project';
 
 export const getProjectList = (dispatch, callback) => {
-    const data = [{
-        id: 'id1',
-        name: 'project1',
-    },{
-        id: 'id2',
-        name: 'project2',
-    }
-    ];
-    dispatch(setProjectList(data));
-    callback(STATUS.SUCCESS);
+    httpGet(projectBase, (result) => {
+        const {status, data} = result;
+        if (status === STATUS.SUCCESS) {
+            dispatch(setProjectList(data));
+        }
+        callback && callback(status);
+    });
 };
 
 export const getProjectContent = (dispatch, id, callback) => {
-
+    httpGet(projectBase + '/' + id, (result) => {
+        const {status, data} = result;
+        if (status === STATUS.SUCCESS) {
+            dispatch(setProjectContent(data));
+        }
+        callback && callback(status);
+    });
 };
 
 export const deleteProject = (dispatch, id, callback) => {
-
+    httpDelete(projectBase, {id:id}, (result) => {
+        const {status} = result;
+        if (status === STATUS.SUCCESS) {
+            httpGet(projectBase, (result) => {
+                dispatch(removeProject(id));
+            });
+        }
+        callback && callback(status);
+    });
 };
 
 export const newProject = (dispatch, projectData, callback) => {
-
+    httpPost(projectBase, projectData, (result) => {
+        const {data, status} = result;
+        if (status === STATUS.SUCCESS) {
+            dispatch(setProjectContent(data));
+        }
+        callback && callback(status);
+    });
 };
 
 export const updateProject = (dispatch, projectData, callback) => {
-
+    httpPut(projectBase, projectData, (result) => {
+        const {status, data} = result;
+        if (status === STATUS.SUCCESS) {
+            dispatch(setProjectContent(data));
+        }
+        callback && callback(status);
+    });
 };
