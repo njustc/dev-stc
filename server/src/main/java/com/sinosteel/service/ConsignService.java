91d2bc6efd507cc60bbae49248145682300aba64
tangcong <1195882464@qq.com>
2018-05-24 10:56:10 +0800

    fix bug

--- server/src/main/java/com/sinosteel/service/ConsignService.java
+++ server/src/main/java/com/sinosteel/service/ConsignService.java
@@ -3,6 +3,7 @@ package com.sinosteel.service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.sinosteel.activiti.ProcessInstanceUpdater;
 import com.sinosteel.domain.Consign;
 import com.sinosteel.domain.User;
 import com.sinosteel.repository.ConsignRepository;
@@ -24,7 +25,7 @@ public class ConsignService extends BaseService<Consign> {
     private ConsignRepository consignRepository;
 
     @Autowired
-    private ProcessInstanceService processInstanceService;
+    private ProcessInstanceUpdater processInstanceUpdater;
 
 
 
@@ -79,7 +80,7 @@ public class ConsignService extends BaseService<Consign> {
         consign.setUser(user);
 
         //start activiti process
-        String procID = processInstanceService.createConsignProcess(params, user);
+        String procID = processInstanceUpdater.createConsignProcess(params, user);
         consign.setProcessInstanceID(procID);
         this.saveEntity(consign, user);
 
@@ -101,7 +102,7 @@ public class ConsignService extends BaseService<Consign> {
 
     private JSONObject processConsign(Consign consign) throws Exception {
         //增加委托状态
-        String processState = (String)processInstanceService.queryProcessState(consign.getProcessInstanceID()).get("state");
+        String processState = (String) processInstanceUpdater.queryProcessState(consign.getProcessInstanceID()).get("state");
         JSONObject jsonObject = JSON.parseObject(JSONObject.toJSONString(consign));
         jsonObject.put("state", processState);
         return jsonObject;
@@ -114,7 +115,7 @@ public class ConsignService extends BaseService<Consign> {
         for (Consign consign: consigns) {
             JSONObject jsonObject = JSON.parseObject(JSONObject.toJSONString(consign));
             jsonObject.remove("consignation");
-            String processState = (String)processInstanceService.queryProcessState(consign.getProcessInstanceID()).get("state");
+            String processState = (String) processInstanceUpdater.queryProcessState(consign.getProcessInstanceID()).get("state");
             jsonObject.put("state", processState);
             resultArray.add(jsonObject);
         }
