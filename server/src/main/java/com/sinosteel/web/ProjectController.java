0c574179d451ce33a570506e994c942978863988
sunquanwei <357505402@qq.com>
2018-06-02 19:10:54 +0800

    ProjectController&&Service

--- server/src/main/java/com/sinosteel/web/ProjectController.java
+++ server/src/main/java/com/sinosteel/web/ProjectController.java
@@ -5,14 +5,13 @@ import com.sinosteel.framework.core.web.Response;
 import com.sinosteel.framework.core.web.ResponseType;
 import com.sinosteel.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author SQW
+ * @author LBW&SQW
  */
 
 @RestController
@@ -21,6 +20,7 @@ public class ProjectController extends BaseController{
     @Autowired
     private ProjectService projectService;
 
+    //根据用户查询
     @RequestMapping(value = "/project", method = RequestMethod.GET)
     public Response queryProjects(Request request) {
         Response response = new Response();
@@ -32,6 +32,76 @@ public class ProjectController extends BaseController{
             e.printStackTrace();
             response.status = ResponseType.FAILURE;
             response.message = e.getMessage();
+        }
+
+        return response;
+    }
+
+    //根据id查询工程
+    @RequestMapping(value = "/project/{id}", method = RequestMethod.GET)
+    public Response queryProjectByID(@PathVariable String id, Request request) {
+
+        Response response = new Response();
+
+        try {
+            response.data = projectService.queryProjectById(id);
+            response.status = ResponseType.SUCCESS;
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.message = e.getMessage();
+            response.status = ResponseType.FAILURE;
+        }
+
+        return response;
+    }
+
+    //添加
+    @RequestMapping(value = "/project", method = RequestMethod.POST)
+    public Response addProject(Request request) {
+        Response response = new Response();
+        try {
+            response.data = projectService.addProject(request.getParams(), request.getFiles(), request.getUser());
+            response.status = ResponseType.SUCCESS;
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.message = e.getMessage();
+            response.status = ResponseType.FAILURE;
+        }
+
+        return response;
+    }
+
+    //更新
+    @RequestMapping(value = "/project", method = RequestMethod.PUT)
+    public Response editProject(Request request) {
+
+        Response response = new Response();
+
+        try {
+            response.data = projectService.editProject(request.getParams(), request.getFiles(), request.getUser());
+            response.status = ResponseType.SUCCESS;
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.message = e.getMessage();
+            response.status = ResponseType.FAILURE;
+        }
+
+        return response;
+    }
+
+    //删除
+    @RequestMapping(value = "/project", method = RequestMethod.DELETE)
+    public Response deleteProject(Request request) {
+
+        Response response = new Response();
+
+        try {
+            projectService.deleteProject(request.getParams());
+            response.status = ResponseType.SUCCESS;
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.message = e.getMessage();
+            response.status = ResponseType.FAILURE;
         }
 
         return response;
