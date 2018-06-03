7682820dfbf7cb509e2ec0d36464140f29a41bee
peiyuchen <981991049@qq.com>
2018-06-03 19:30:36 +0800

    modify TestCaseComponent

--- client/framework-webclient/src/routes/Test/components/TestCaseContentComponent.js
+++ client/framework-webclient/src/routes/Test/components/TestCaseContentComponent.js
@@ -1,6 +1,6 @@
 /*测试用例*/
 import React, {Component, PropTypes} from 'react';
-import {Form,Button,Input,Icon, Row, Col, DatePicker,InputNumber} from 'antd'
+import {Form,Table, Card, Badge, Dropdown, Menu, Button,Input,Icon, Row, Col, Popconfirm, DatePicker,InputNumber} from 'antd'
 const FormItem=Form.Item;
 
 let uuid = 0;
@@ -9,118 +9,200 @@ class TestCaseContentComponent extends Component {
         super(props);
     }
 
-    remove = (k) => {
-        const { form } = this.props;
-        // can use data-binding to get
-        const keys = form.getFieldValue('keys');
-        // We need at least one passenger
-        if (keys.length === 1) {
-            return;
-        }
-
-        // can use data-binding to set
-        form.setFieldsValue({
-            keys: keys.filter(key => key !== k),
-        });
+    static propTypes = {
+        //setListFilter: PropTypes.func,
+        dataSource: PropTypes.array,
+        showContent: PropTypes.func,
+        //deleteConsign: PropTypes.func,
+        //getConsignList: PropTypes.func,
+        //newConsign: PropTypes.func,
+        //enableNew: PropTypes.bool,
     };
 
-    add = () => {
-        const { form } = this.props;
-        // can use data-binding to get
-        const keys = form.getFieldValue('keys');
-        const nextKeys = keys.concat(uuid);
-        uuid++;
-        // can use data-binding to set
-        // important! notify form to detect changes
-        form.setFieldsValue({
-            keys: nextKeys,
-        });
+    expandedRowRender = (record) => {
+        const rowStyle = {
+            marginBottom:'10pt',
+        };
+
+        return (
+            <div>
+                <Row style={rowStyle}>
+                    <Col span={3}><b>设计说明</b></Col>
+                    <Col span={21}><p style={{ margin: 0 }}>{record.designNotes}</p></Col>
+                </Row>
+                <Row style={rowStyle}>
+                    <Col span={3}><b>有关的规约说明</b></Col>
+                    <Col span={21}><p style={{ margin: 0 }}>{record.statute}</p></Col>
+                </Row>
+                <Row style={rowStyle}>
+                    <Col span={3}><b>依据</b></Col>
+                    <Col span={21}><p style={{ margin: 0 }}>{record.accordance}</p></Col>
+                </Row>
+            </div>
+        );
     };
+
+    onClick = (buttonIndex) => () => {
+        // this.props.form.validateFields((err, values) => {
+        //     if (!err) {
+        //         this.props.buttons[buttonIndex].onClick(this.props.consignData, JSON.stringify(values));
+        //     }
+        // });
+        const {buttons, form} = this.props;
+        buttons[buttonIndex].onClick(JSON.stringify(form.getFieldsValue()));
+    };
+
+    /*table列设置*/
+    columns = [{
+        title:"序号",
+        dataIndex:"id",
+    }, {
+        title:"测试分类",
+        dataIndex:"classification",
+    },
+        /*{
+        title:"测试用例设计说明",
+        dataIndex:"designNotes",
+    },
+     {
+        title:"与本测试用例有关的规约说明",
+        dataIndex:"statute",
+    },*/
+        {
+        title:"测试用例执行过程",
+        dataIndex:"process",
+    }, {
+        title:"预期的结果",
+        dataIndex:"expectedResult",
+    }, {
+        title:"测试用例设计者",
+        dataIndex:"designer",
+    }, {
+        title:"时间",
+        dataIndex:"time",
+    }, {
+        title:"操作",
+        dataIndex:"action",
+        render: (text, record) => {
+        return (
+            <Popconfirm title="Sure to delete?" onConfirm={() => this.onDelete(record.key)}>
+              <a href="javascript:;">Delete</a>
+            </Popconfirm>
+        );
+      },
+    },/*{
+        title:"依据",
+        dataIndex:"accordance",
+    }*/
+    ];
+
+    /* TODO 删除测试用例 */
+    onDelete = (key) => {
+        // this.props.deleteTestCase(key);
+    }
+
+    data = [{
+        id: 1,
+        classification: 'yj',
+        process: 'unhappy->happy',
+        expectedResult: 'happy',
+        designer: 'yj',
+        time: '2018-06-03',
+        action: 'delete',
+        designNotes: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+        statute: 'sssssss',
+        accordance: 'tttttt'
+    }];
 
     render(){
-        const { getFieldDecorator, getFieldValue } = this.props.form;
-        const formItemLayout = {
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout =  {
             labelCol: { span: 4 },
             wrapperCol: { span: 20 },
         };
 
-        const formItemLayoutWithOutLabel = {
-            wrapperCol: { span: 24, offset: 0 },
-        };
-
-        const rowStyle = {
-            height: '40px',
-            lineHeight: '40px',
-            marginTop: 0,
-            marginBottom: 5,
-        };
-
-        const gridHeadStyle = {
-            background: '#cccccc',
-            height: '100%',
-            textAlign: 'center',
-        };
-
-        getFieldDecorator('keys', { initialValue: [] });
-        const keys = getFieldValue('keys');
-        const formItems = keys.map((k, index) => {
-            return (
-                <FormItem
-                    {...formItemLayoutWithOutLabel}
-                    required={false}
-                    key={k}
-                >
-                    {getFieldDecorator(`names[${k}]`, {
-                        // validateTrigger: ['onChange', 'onBlur'],
-                        rules: [{
-                            required: true,
-                            whitespace: true,
-                            message: "Please input passenger's name or delete this field.",
-                        }],
-                    })(
-                        <Row>
-                            <Col span={5}>
-                                <Input placeholder="passenger name" style={{marginRight: 8 }} />
-                            </Col>
-                            <Col span={5}>
-                                <Input placeholder="user name" style={{marginRight: 8 }} />
-                            </Col>
-                            <Col span={1}>
-                                <Icon
-                                    className="dynamic-delete-button"
-                                    type="minus-circle-o"
-                                    disabled={keys.length === 1}
-                                    onClick={() => this.remove(k)}
-                                />
-                            </Col>
-                        </Row>
-                    )}
-                    </FormItem>
-            );
-        });
-        return(
-            <Form onSubmit={this.handleSubmit}>
-                {formItems}
-                <FormItem>
-                    <Row style = {rowStyle} type="flex" justify="center" gutter={2}>
-                        <Col span={2}><h3 style={gridHeadStyle}>测试分类</h3></Col>
-                        <Col span={1}><h3 style={gridHeadStyle}>序号</h3></Col>
-                        <Col span={2}><h3 style={gridHeadStyle}>测试用例设计说明</h3></Col>
-                        <Col span={4}><h3 style={gridHeadStyle}>与本测试用例有关的规约说明</h3></Col>
-                        <Col span={4}><h3 style={gridHeadStyle}>测试用例执行过程</h3></Col>
-                        <Col span={3}><h3 style={gridHeadStyle}>预期的结果</h3></Col>
-                        <Col span={2}><h3 style={gridHeadStyle}>测试用例设计者</h3></Col>
-                        <Col span={2}><h3 style={gridHeadStyle}>时间</h3></Col>
-                        <Col span={4}><h3 style={gridHeadStyle}>依据</h3></Col>
-                    </Row>
-                </FormItem>
-                <FormItem {...formItemLayoutWithOutLabel}>
-                    <Button type="dashed" onClick={this.add} style={{ width: '100%' }}>
-                        <Icon type="plus" /> 添加测试用例
-                    </Button>
-                </FormItem>
-            </Form>
-        )
+        return (
+            <div>
+                <h3 style={{ marginBottom: 16 }}>测试用例</h3>
+                <div style={{ background: '#ECECEC', padding: '30px', marginBottom:'10pt' }}>
+                    <Card title="添加测试用例" bordered={false} style={{ width: '100%' }}>
+                        <Form onSubmit={this.handleSubmit} hideRequiredMark={true}>
+                            <FormItem {...formItemLayout} label={"测试分类"}>
+                                {getFieldDecorator('classification', {
+                                    // rules: [{ required: true, message: '请正确输入委托单位！' ,pattern:"^[\u4E00-\u9FA5]+$"}],
+                                })(
+                                    <Input/>
+                                )}
+                            </FormItem>
+                            <FormItem {...formItemLayout} label={"测试用例设计者"}>
+                                {getFieldDecorator('designer', {
+                                    // rules: [{ required: true, message: '请正确输入委托单位！' ,pattern:"^[\u4E00-\u9FA5]+$"}],
+                                })(
+                                    <Input/>
+                                )}
+                            </FormItem>
+                            <FormItem {...formItemLayout} label={"测试用例设计说明"}>
+                                {getFieldDecorator('designNotes', {
+                                    // rules: [{ required: true, message: '请正确输入委托单位！' ,pattern:"^[\u4E00-\u9FA5]+$"}],
+                                })(
+                                    <Input/>
+                                )}
+                            </FormItem>
+                            <FormItem {...formItemLayout} label={"测试用例执行过程"}>
+                                {getFieldDecorator('process', {
+                                    // rules: [{ required: true, message: '请正确输入委托单位！' ,pattern:"^[\u4E00-\u9FA5]+$"}],
+                                })(
+                                    <Input/>
+                                )}
+                            </FormItem>
+                            <FormItem {...formItemLayout} label={"预期的结果"}>
+                                {getFieldDecorator('expectedResult', {
+                                    // rules: [{ required: true, message: '请正确输入委托单位！' ,pattern:"^[\u4E00-\u9FA5]+$"}],
+                                })(
+                                    <Input/>
+                                )}
+                            </FormItem>
+                            <FormItem {...formItemLayout} label={"时间"}>
+                                {getFieldDecorator('time', {
+                                    // rules: [{ required: true, message: '请正确输入委托单位！' ,pattern:"^[\u4E00-\u9FA5]+$"}],
+                                })(
+                                    <Input/>
+                                )}
+                            </FormItem>
+                            <FormItem{...formItemLayout}label={"与本测试用例有关的规约说明"}>
+                                {getFieldDecorator('statute', {
+                                    // rules: [{ required: true, message: '请正确输入委托单位！' ,pattern:"^[\u4E00-\u9FA5]+$"}],
+                                })(
+                                    <Input/>
+                                )}
+                            </FormItem>
+                            <FormItem {...formItemLayout} label={"依据"}>
+                                {getFieldDecorator('accordance', {
+                                    // rules: [{ required: true, message: '请正确输入委托单位！' ,pattern:"^[\u4E00-\u9FA5]+$"}],
+                                })(
+                                    <Input/>
+                                )}
+                            </FormItem>
+                            <FormItem {...formItemLayout}>
+                                {this.props.buttons.map((button, index) =>
+                                    <Button onClick={this.onClick(index)}
+                                            key={button.content}>
+                                        {button.content}
+                                    </Button>)}
+                            </FormItem>
+                        </Form>
+                    </Card>
+                </div>
+                <Table
+                    className="components-table-demo-nested"
+                    columns={this.columns}
+                    expandedRowRender={this.expandedRowRender}
+                    // expandedRowRender={record => <p style={{ margin: 0 }}>{record.description}</p>}
+                    dataSource={this.data}
+                    rowKey={'id'}
+                />
+            </div>
+        );
     }
 }
 
