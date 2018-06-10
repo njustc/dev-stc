ed1983aba664c8c7ded0ab5e912415e7a35e6b26
tangcong <1195882464@qq.com>
2018-06-10 09:08:13 +0800

    activiti初步更新 增加用户当前任务的权限信息、完成所有流程

--- server/src/main/java/com/sinosteel/activiti/TCProcessEngine.java
+++ server/src/main/java/com/sinosteel/activiti/TCProcessEngine.java
@@ -1,14 +1,11 @@
 package com.sinosteel.activiti;
 
 import com.alibaba.fastjson.JSONObject;
-import com.sinosteel.domain.User;
 import com.sinosteel.framework.core.web.Request;
 import com.sinosteel.framework.mybatis.UserMapper;
-import org.activiti.bpmn.model.BpmnModel;
-import org.activiti.bpmn.model.FlowElement;
-import org.activiti.bpmn.model.SequenceFlow;
-import org.activiti.bpmn.model.UserTask;
 import org.activiti.engine.*;
+import org.activiti.engine.form.FormProperty;
+import org.activiti.engine.form.TaskFormData;
 import org.activiti.engine.history.HistoricActivityInstance;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
@@ -29,16 +26,23 @@ public class TCProcessEngine {
     HistoryService historyService;
 
     @Autowired
-    RepositoryService repositoryService;
-    @Autowired
-    ProcessEngine processEngine;
+    FormService formService;
+
     @Autowired
     private BaseOperation baseOperation;
 
-    enum TaskOperation{submit,review,confirm,write,implement}
-
+    enum TaskOperation{Submit,Review,Confirm,Write,Implement,
+                Approve,Send,Done,Fil,Satisfact}
+   /* enum TaskNoGate{submit,write,implement,send,done,file,satisfact}
+    enum TaskContainGate{review,confirm,approve}
+*/
     @Autowired
     private UserMapper userMapper;
+
+    public void setUp()throws Exception
+    {
+
+    }
     /**
      * TODO 将用户组传入流程实例
      *  新建一个新的委托实例
@@ -85,19 +89,32 @@ public class TCProcessEngine {
         return pi.getProcessInstanceId();
     }
 
-    public String createTestplanProcess(String testPlanId, String workerId)throws Exception{
+    public String createTestplanProcess()throws Exception{
         Map<String,Object> variables=new HashMap<String, Object>();
-        //variables.put("testPlanID",testPlanId);
-        //variables.put("testEmployerId",workerId);
         List<String> userids = userMapper.getUserIdsByRoleId("1");
         if(userids.isEmpty()==false) {
             for(String userid : userids){
-                variables.put("WorkerIDs",userid);
+                variables.put("WorkerId",userid);
             }
         }
         else
             throw new Exception("EMPTY");
         ProcessInstance pi=runtimeService.startProcessInstanceByKey("testplan",variables);
+        return pi.getProcessInstanceId();
+    }
+
+    public String createTestreportProcess()throws Exception
+    {
+        Map<String,Object> variables=new HashMap<String, Object>();
+        List<String> userids = userMapper.getUserIdsByRoleId("1");
+        if(userids.isEmpty()==false) {
+            for(String userid : userids){
+                variables.put("WorkerId",userid);
+            }
+        }
+        else
+            throw new Exception("EMPTY");
+        ProcessInstance pi=runtimeService.startProcessInstanceByKey("testreport",variables);
         return pi.getProcessInstanceId();
     }
     /**
@@ -114,10 +131,13 @@ public class TCProcessEngine {
         if (object == null) {
             throw new Exception("object is null");
         }
-        if(operation.contains(TaskOperation.submit.name())||operation.contains(TaskOperation.write.name())
-                ||operation.contains(TaskOperation.implement.name())) {
+        if(operation.contains(TaskOperation.Submit.name())||operation.contains(TaskOperation.Write.name())
+                ||operation.contains(TaskOperation.Implement.name())||operation.contains(TaskOperation.Send.name())
+                ||operation.contains(TaskOperation.Done.name())||operation.contains(TaskOperation.Fil.name())
+                ||operation.contains(TaskOperation.Satisfact.name())) {
             baseOperation.noGate(processInstanceId,request.getUser().getId());}
-        else if(operation.contains(TaskOperation.review.name())||operation.contains(TaskOperation.confirm.name())) {
+        else if(operation.contains(TaskOperation.Review.name())||operation.contains(TaskOperation.Confirm.name())
+                ||operation.contains(TaskOperation.Approve.name())) {
             baseOperation.containGate(operation,processInstanceId,request.getUser().getId());}
         else{
             throw new Exception("Operation match failed");}
@@ -147,6 +167,44 @@ public class TCProcessEngine {
             return "NotExist";}
     }
 
-    public void getUserOperation(String processInstanceId) throws Exception
-    { }
+    /*当前task用户可执行的权限*/
+    public List<String> getUserOperation(String processInstanceId) throws Exception
+    {
+        List<String> varies=new ArrayList<String>() ;
+        Task task=taskService.createTaskQuery()
+                .processInstanceId(processInstanceId).singleResult();
+        String S=this.getProcessState(processInstanceId);
+        if(S.equals("Finished")) {
+            varies.add("Finish");
+            return varies;
+        }
+        TaskFormData taskFormData=formService.getTaskFormData(task.getId());
+        List<FormProperty> formProperties=taskFormData.getFormProperties();
+        if(formProperties.isEmpty()==true) {
+            for(TaskOperation s:TaskOperation.values()) {
+                if(task.getName().contains(s.name())) {
+                    varies.add(s.name());
+                    break;
+                }
+            }
+            return varies;
+        }
+        else {
+            for(TaskOperation s:TaskOperation.values()) {
+                if(task.getName().contains(s.name())) {
+                    String s1=s.name()+"Pass";
+                    varies.add(s1);
+                    String s2=s.name()+"Reject";
+                    varies.add(s2);
+                    //System.out.println(task.getProcessDefinitionId());
+                    if(s.name()=="Confirm"&&task.getProcessDefinitionId().contains("testreport")) {
+                        String s3=s.name()+"Abort";
+                        varies.add(s3);
+                    }
+                    break;
+                }
+            }
+            return varies;
+        }
+    }
 }
--- server/src/main/java/com/sinosteel/service/TestPlanService.java
+++ server/src/main/java/com/sinosteel/service/TestPlanService.java
@@ -4,10 +4,10 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.sinosteel.activiti.ProcessInstanceService;
-import com.sinosteel.domain.TestPlan;
+import com.sinosteel.domain.TestCase;
 import com.sinosteel.domain.User;
 import com.sinosteel.domain.Project;
-import com.sinosteel.repository.TestPlanRepository;
+import com.sinosteel.repository.TestCaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,9 +20,9 @@ import java.util.UUID;
  */
 
 @Service
-public class TestPlanService extends BaseService<TestPlan> {
+public class TestPlanService extends BaseService<TestCase> {
     @Autowired
-    private TestPlanRepository testplanRepository;
+    private TestCaseRepository testCaseRepository;
 
     @Autowired
     private ProcessInstanceService processInstanceService;
@@ -47,46 +47,46 @@ public class TestPlanService extends BaseService<TestPlan> {
     }*/
 
     public JSONObject queryTestPlanByID(String id) throws Exception{
-        TestPlan testplan = testplanRepository.findById(id);
-        if (testplan == null)
+        TestCase testCase = testCaseRepository.findById(id);
+        if (testCase == null)
             throw new Exception("Not found");
-        return JSON.parseObject(JSONObject.toJSONString(testplan));
+        return JSON.parseObject(JSONObject.toJSONString(testCase));
     }
 
-    //改动测试计划
+/*    //改动测试计划
     public JSONObject editTestPlan(JSONObject params, List<MultipartFile> files, User user) throws Exception
     {
-        TestPlan temptestplan = JSONObject.toJavaObject(params, TestPlan.class);
-        TestPlan testplan;
-        if ((testplan = this.findEntityById(temptestplan.getId())) == null) {
+        TestCase temptestplan = JSONObject.toJavaObject(params, TestCase.class);
+        TestCase testCase;
+        if ((testCase = this.findEntityById(temptestplan.getId())) == null) {
             throw new Exception("Not found");
         }
         //编辑测试计划时时只编辑内容
-        testplan.setPlan(temptestplan.getPlan());
+        testCase.setPlan(temptestplan.getPlan());
         this.updateEntity(testplan, user);
 
         //TODO:return the consign with STATE!
         testplan = testplanRepository.findById(temptestplan.getId());
         return processTestPlan(testplan);
-    }
+    }*/
 
     //增加测试计划
     public JSONObject addTestPlan(JSONObject params,List<MultipartFile> files,User user) throws Exception {
 
         String uid=UUID.randomUUID().toString();
 
-        TestPlan testplan=JSONObject.toJavaObject(params,TestPlan.class);
-        testplan.setId(uid);
+        TestCase testCase=JSONObject.toJavaObject(params,TestCase.class);
+        testCase.setId(uid);
         /*testplan.setUser(user);*/
 
         //TODO:start activiti process
        // String procID = processInstanceService.createTestPlanProcess(params, user);
         //testplan.setProcessInstanceID(procID);
-        this.saveEntity(testplan, user);
+        this.saveEntity(testCase, user);
 
         //TODO:添加testplan状态
-        testplan = testplanRepository.findById(uid);
-        return processTestPlan(testplan);
+        testCase = testCaseRepository.findById(uid);
+        return processTestPlan(testCase);
     }
 
 
@@ -100,19 +100,19 @@ public class TestPlanService extends BaseService<TestPlan> {
 
 
     //TODO:增加测试计划状态
-    private JSONObject processTestPlan(TestPlan testplan) throws Exception {
+    private JSONObject processTestPlan(TestCase testCase) throws Exception {
         //String processState = (String) processInstanceService.queryProcessState(testplan.getProcessInstanceID()).get("state");
-        JSONObject jsonObject = JSON.parseObject(JSONObject.toJSONString(testplan));
+        JSONObject jsonObject = JSON.parseObject(JSONObject.toJSONString(testCase));
         //jsonObject.put("state", processState);
         return jsonObject;
 
     }
 
     //去掉测试计划内容,TODO:添加状态
-    private  JSONArray processTestPlans(List<TestPlan> testplans) throws Exception {
+    private  JSONArray processTestPlans(List<TestCase> testplans) throws Exception {
         JSONArray resultArray = new JSONArray();
-        for (TestPlan testplan: testplans) {
-            JSONObject jsonObject = JSON.parseObject(JSONObject.toJSONString(testplan));
+        for (TestCase testCase: testplans) {
+            JSONObject jsonObject = JSON.parseObject(JSONObject.toJSONString(testCase));
             jsonObject.remove("testplan");
             //String processState = (String) processInstanceService.queryProcessState(testplan.getProcessInstanceID()).get("state");
             //jsonObject.put("state", processState);
