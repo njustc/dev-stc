062cd45efd15167be202116f2affe258ca22ccde
Chun Cao <caochun@gmail.com>
2018-06-28 11:41:26 +0800

    some formatting

--- server/src/main/java/com/sinosteel/domain/Contract.java
+++ server/src/main/java/com/sinosteel/domain/Contract.java
@@ -1,6 +1,5 @@
 package com.sinosteel.domain;
 
-
 import com.alibaba.fastjson.annotation.JSONField;
 
 import javax.persistence.*;
@@ -16,48 +15,48 @@ public class Contract extends BaseEntity {
     private String contractBody;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "USER_ID",foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT))
+    @JoinColumn(name = "USER_ID", foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT))
     @JSONField(serialize = false)
     private User user;
-    public User getUser()
-    {
+
+    public User getUser() {
         return user;
     }
 
-    public void setUser(User user)
-    {
+    public void setUser(User user) {
         this.user = user;
     }
 
     @Column(name = "PROCESS_INSTANCE_ID")
     private String processInstanceID;
-    public String getProcessInstanceID()
-    {
+
+    public String getProcessInstanceID() {
         return processInstanceID;
     }
-    public void setProcessInstanceID(String processInstanceID){this.processInstanceID = processInstanceID;}
 
-    //合同所在的工程
+    public void setProcessInstanceID(String processInstanceID) {
+        this.processInstanceID = processInstanceID;
+    }
+
+    // 合同所在的工程
     @OneToOne(mappedBy = "contract")
     @JSONField(serialize = false)
     private Project project;
-    public Project getProject(){
+
+    public Project getProject() {
         return project;
     }
 
-    public void setProject(Project project){
+    public void setProject(Project project) {
         this.project = project;
     }
-    //===================================
+    // ===================================
 
-
-    public String getContractBody()
-    {
+    public String getContractBody() {
         return contractBody;
     }
 
-    public void setContractBody(String contractBody)
-    {
+    public void setContractBody(String contractBody) {
         this.contractBody = contractBody;
     }
 }
