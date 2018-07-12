53f8fede5b4da0b8a37b310b5d8f9ed1dbcad057
homesettler <13255527940@163.com>
2018-07-12 10:33:29 +0800

    add Unit Test

--- server/src/test/java/com/sinosteel/Repository/ProjectRepositoryTests.java
+++ server/src/test/java/com/sinosteel/Repository/ProjectRepositoryTests.java
@@ -1,9 +1,8 @@
 package com.sinosteel.Repository;
 
+
 import com.sinosteel.FrameworkApplication;
-import com.sinosteel.domain.Contract;
-import com.sinosteel.repository.ContractRepository;
-import org.junit.Assert;
+import com.sinosteel.repository.ProjectRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +10,17 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.transaction.Transactional;
-import java.util.List;
-
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(FrameworkApplication.class)
-public class ContractRepositoryTests {
+public class ProjectRepositoryTests {
 
     @Autowired
-    private ContractRepository contractRepository;
+    private ProjectRepository projectRepository;
 
     @Test
     @Transactional
-    public void testContractProject()
-    {
+    public void testProject(){
 
     }
-
 }
