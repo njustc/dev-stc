0befb1eddfa12a13255dc382aa0dff238a71ae2b
YangJieNJU <151220140@smail.nju.edu.cn>
2018-07-16 10:19:49 +0800

    add some comments

--- client/framework-webclient/src/utils/FetchUtil.js
+++ client/framework-webclient/src/utils/FetchUtil.js
@@ -1,21 +1,56 @@
 import {STATUS} from "SERVICES/common";
 
+/**
+ * 实现向后台发送GET请求
+ * @param {*} url username、clientDigest之前的url部分
+ * @param {*} callback 回调内容为GET请求传输结果的状态，可能为SUCCESS或FAILURE
+ * @param {*} urlParams uesename、clientDigest之后的url部分（不一定都有）
+ */
 export const httpGet = (url, callback, urlParams) => {
     return sysFetch('GET', url, null, callback, urlParams);
 };
 
+/**
+ * 实现向后台发送POST请求
+ * @param {*} url username、clientDigest之前的url部分
+ * @param {*} params 需要向后台传输的数据，格式为JSON
+ * @param {*} callback 回调内容为POST请求传输结果的状态，可能为SUCCESS或FAILURE
+ * @param {*} urlParams uesename、clientDigest之后的url部分（不一定都有）
+ */
 export const httpPost = (url, params, callback, urlParams) => {
     return sysFetch('POST', url, params, callback, urlParams);
 };
 
+/**
+ * 实现向后台发送PUT请求
+ * @param {*} url username、clientDigest之前的url部分
+ * @param {*} params 需要向后台传输的数据，格式为JSON
+ * @param {*} callback 回调内容为POST请求传输结果的状态，可能为SUCCESS或FAILURE
+ * @param {*} urlParams uesename、clientDigest之后的url部分（不一定都有）
+ */
 export const httpPut = (url, params, callback, urlParams) => {
     return sysFetch('PUT', url, params, callback, urlParams);
 };
 
+/**
+ * 实现向后台发送DELETE请求
+ * @param {*} url username、clientDigest之前的url部分
+ * @param {*} params 需要向后台传输的数据，格式为JSON
+ * @param {*} callback 回调内容为POST请求传输结果的状态，可能为SUCCESS或FAILURE
+ * @param {*} urlParams uesename、clientDigest之后的url部分（不一定都有）
+ */
 export const httpDelete = (url, params, callback, urlParams) => {
     return sysFetch('DELETE', url, params, callback, urlParams);
 };
 
+/**
+ * 各http请求方法通过调用此方法实现向后台发送fetch请求
+ * @param {*} Method 用来标识http方法的种类，可能为POST、PUT、GET、DELETE
+ * @param {*} url username、clientDigest之前的url部分
+ * @param {*} params 需要向后台传输的数据，格式为JSON
+ * @param {*} callback 回调内容为与后台数据传输结果的状态，可能为SUCCESS或FAILURE
+ * @param {*} urlParams uesename、clientDigest之后的url部分（不一定都有）
+ */
 const sysFetch = (Method, url, params, callback, urlParams) => {
 
     let result = { 
@@ -45,13 +80,6 @@ const sysFetch = (Method, url, params, callback, urlParams) => {
     if (Method != 'GET') {
         request.body = formData;
     }
-
-    //
-    if(Method == 'DELETE'){
-        console.log(fullUrl);
-        console.log(request);
-    }
-    //
 
     return fetch(fullUrl, request)
         .then((res) =>
