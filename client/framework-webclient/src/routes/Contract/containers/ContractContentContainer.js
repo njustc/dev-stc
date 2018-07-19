048d825ee33e1c2213036aa3276f5ce0c72d5654
Benjamin15122 <15051183877@163.com>
2018-07-19 15:42:11 +0800

    fix contract

--- client/framework-webclient/src/routes/Contract/containers/ContractContentContainer.js
+++ client/framework-webclient/src/routes/Contract/containers/ContractContentContainer.js
@@ -27,7 +27,7 @@ const mapStateToProps = (state, ownProps) => {
     return {
         contractData: content?content:ownProps,
         values:  contractBody ? JSON.parse(contractBody) : {},
-        disable: contractState!=="TobeSubmit",
+        disable: isMarketing&&contractState!=="TobeSubmit",
         buttonsEnable: buttonsEnable(isCustomer,isMarketing,isSubmitVisible,isReviewVisible,isConfirmVisible),
     }
 };
